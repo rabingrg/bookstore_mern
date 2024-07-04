665105ec73cb6795bb5df8d9cf4530dc1d44dfc0
Rabin Gurung <wrabeeng@gmail.com>
2024-07-04 10:35:14 +0545

    refactor: issues

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // envPrefix: "RABIN_",
+  // envPrefix: "RABIN_"/"_VAROSA",
   server: {
     open: true,
   },
